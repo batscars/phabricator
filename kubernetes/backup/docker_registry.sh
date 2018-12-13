0da09cf9cf74d6bbe92cb17a317b4e67daf2c27c
zhangbin <battlescars@qq.com>
2018-12-13 21:16:22 +0800

    modify dockerfile

---
+++ kubernetes/backup/docker_registry.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+kubectl create secret docker-registry ali-secret -n aitech --docker-server=registry.cn-shenzhen.aliyuncs.com --docker-username=lzywbh1993 --docker-password=***west --docker-email=battlescars@qq.com
